e8aa4e4ae4b72c9b63448ae0d3a73cd160a94d27
clement <maboiteaspam@wanadoo.fr>
2015-12-19 18:56:27 +0800

    update functio nsignature to allow lighter requirements

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var flower = require('flower')
 
 module.exports = function bubbled (name, events, fnT, fnF) {
   var args = [].slice.apply(arguments);
+  if (args.length===0) return bubbler(flower(), ['messages']);
+  if (args.length===1) return bubbler(flower(name), ['messages']);
+  if (args.length===2) return bubbler(flower(name, events), ['messages']);
   if (args.length===3) return bubbler(flower(events, fnF), name);
   return bubbler(flower(fnT, fnF), events, name);
 };
